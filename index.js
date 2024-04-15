abd2fb973b612bddada8901d6fda80f992b78470
subramaniyan <116158768+SubramaniyanGS@users.noreply.github.com>
2024-04-15 20:27:46 +0530

    Update index.js
    save

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Serve static files from the 'static' directory
-app.use('/static/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use('/static/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/static/uploads', express.static(path.join(__dirname, 'public/uploads')));
+app.use('/static/assets', express.static(path.join(__dirname, 'public/assets')));
 
 
 app.use(express.json());
